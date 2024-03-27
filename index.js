7805e1c423595401443d1642c0175988dd3077bd
Barmao Brian <barmaobrian5@gmail.com>
2024-03-27 12:54:55 +0300

    control-lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+let x = 1000;
+function scuberGreetingForFeet(distance) {
+  if (distance <= 1000) {
+    return 'This one is on me!';
+
+  } else if (distance > 1000 && distance <= 1600) {
+
+    return 'That will be twenty bucks.';
+
+  } else if (distance > 1600 && distance <= 3400) {
+
+    return 'I will gladly take your thirty bucks.';
+
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+console.log(scuberGreetingForFeet(x));
+
+let city = 'Nairobi';
+city = 'Nairobi';
+function ternaryCheckCity(city) {
+
+
+  return city === 'Nairobi' ? 'Ok, sounds good.' : 'No go.';
+}
+console.log(ternaryCheckCity(city));
+
+let tips= 'generous';
+function switchOnCharmFromTip(tips) {
+switch(tips) 
+{
+  case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous':
+   return 'Thank you.';
+  default:
+  return 'Bye.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+console.log(switchOnCharmFromTip(tips));
